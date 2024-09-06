dd187cd85475ff7c309c521afd0fdd32eeaa217a
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-09-06 11:33:08 +0530

    final commit for counter ap

--- Projects/Project-1 Counter App/counter-app/src/App.js
+++ Projects/Project-1 Counter App/counter-app/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import Counter from './Components/Counter'
+import Counter from './Components/Counter/index'
 
 const App = () =>{
   return <Counter/>
